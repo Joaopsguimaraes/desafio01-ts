84050313adcf30af6cb0fce27c92e03e3a6b874e
Joao Guimaraes <psjoaovictor@gmail.com>
2024-06-10 15:50:17 -0300

    feat: :sparkles: new challenge features

--- app.ts
+++ app.ts
@@ -1,9 +1,13 @@
 import { CompanyAccount } from './class/CompanyAccount'
+import { NewAccount } from './class/NewAccount'
 import { PeopleAccount } from './class/PeopleAccount'
 
 const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
 console.log(peopleAccount)
-peopleAccount.deposit()
+peopleAccount.deposit(20)
 const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-companyAccount.deposit()
+companyAccount.deposit(50)
 console.log(companyAccount)
+const newAccountTest = new NewAccount('Nath', 10)
+newAccountTest.newDeposit(10)
+console.log(newAccountTest)
--- class/DioAccount.ts
+++ class/DioAccount.ts
@@ -1,7 +1,7 @@
 export abstract class DioAccount {
-  private name: string
+  private readonly name: string
   private readonly accountNumber: number
-  balance: number = 0
+  protected balance: number = 0
   private status: boolean = true
 
   constructor(name: string, accountNumber: number){
@@ -9,30 +9,29 @@ export abstract class DioAccount {
     this.accountNumber = accountNumber
   }
 
-  setName = (name: string): void => {
-    this.name = name
-    console.log('Nome alterado com sucesso!')
-  }
-
   getName = (): string => {
     return this.name
   }
 
-  deposit = (): void => {
+  deposit = (value: number): void => {
     if(this.validateStatus()){
+      this.balance += value
       console.log('Voce depositou')
     }
   }
 
-  withdraw = (): void => {
-    console.log('Voce sacou')
+  withdraw = (value: number): void => {
+    if(this.validateStatus() && this.balance >= value){
+      this.balance -= value
+      console.log('Voce sacou')
+    }
   }
 
   getBalance = (): void => {
     console.log(this.balance)
   }
 
-  private validateStatus = (): boolean => {
+  protected validateStatus = (): boolean => {
     if (this.status) {
       return this.status
     }
